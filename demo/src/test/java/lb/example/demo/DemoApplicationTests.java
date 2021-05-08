e214be48e07108a9f445fb25edb12783cefc7f43
Light-Bearing <hilo@list.ru>
2021-05-08 22:58:10 +0300

    init coomit

---
+++ demo/src/test/java/lb/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lb.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
